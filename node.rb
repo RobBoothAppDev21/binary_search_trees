b55883e7cff350c7e40b41b15f4e2051e8397461
Rob Young <rhyoung214@gmail.com>
2022-03-04 15:10:58 -0600

    Updated node class with symbols for new instance

--- node.rb
+++ node.rb
@@ -3,10 +3,9 @@
 # Node class with three attributes: data, left children, right children
 class Node
 
-  def initialize(data, left_child = nil, right_child = nil)
+  def initialize(data:, left_child: nil, right_child: nil)
     @data = data
     @left_child = left_child
     @right_child = right_child
   end
-
 end
